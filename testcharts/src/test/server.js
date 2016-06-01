55c9b3934f99fe32888667de72264da761dd3344
lpatan <liyakathpatan@gmail.com>
2016-05-31 21:26:06 -0400

    Update server.js

--- testcharts/src/test/server.js
+++ testcharts/src/test/server.js
@@ -6,15 +6,21 @@
     var bodyParser = require('body-parser');    // pull information from HTML
 												// POST (express4)
     var fs = require('fs');
-    
+
     // configuration =================
-    app.use(express.static('../main'));                
-    app.use(morgan('dev'));  
+    app.use(express.static('../main'));
+    app.use(morgan('dev'));
     app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     app.use(bodyParser.json());                                     // parse application/json
     app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    
+
     // listen (start app with node server.js)
 	// ======================================
-    app.listen(3100);
-    console.log("App listening on port 3100");  
+    app.listen(3000);
+    console.log("App listening on port 3000");
+
+   app.get('/service/item/', function(req, res) {
+       var data = fs.readFileSync('testJson/item-data.json', 'utf8');
+       var obj = JSON.parse(data);
+       res.json(obj);
+   });
